12c2b2b28cd2f4897c9778393017835a55ed1aa5
JJJ <jingfu99@gmail.com>
2017-09-14 19:01:26 +0800

    即時顯示經過時間

--- time_tracker.py
+++ time_tracker.py
@@ -1,9 +1,32 @@
-__author__ = 'jjj'
+#!/usr/bin/python
 # -*- coding:utf8 -*-
+__author__ = 'jjj'
 import time
 import datetime
 from os.path import expanduser
 import os
+import threading
+
+input = None
+
+# format the time_delta object by divide the total seconds
+def strfdelta(tdelta, fmt):
+    d = {}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+
+    return fmt.format(**d)
+
+def timer(task):
+    a = datetime.datetime.now()
+    while input is None:
+        _ = os.system('clear')
+        b = datetime.datetime.now()
+        print "Working on %s" %(task)
+        c = b - a
+        print strfdelta(c, "Duration {hours}:{minutes}:{seconds}")
+        time.sleep(1)
+
 
 class Tracker:
 
@@ -20,13 +43,16 @@ class Tracker:
 
     # do the math, and print the result
     def cal(self, a, b, fd):
-        print a.strftime('%H:%M:%S') + ' - ' + b.strftime('%H:%M:%S')
-        fd.write(a.strftime('%H:%M:%S') + ' - ' + b.strftime('%H:%M:%S') + "\n")
+        if fd is not None:
+            print a.strftime('%H:%M:%S') + ' - ' + b.strftime('%H:%M:%S')
+            fd.write(a.strftime('%H:%M:%S') + ' - ' + b.strftime('%H:%M:%S') + "\n")
         c = b - a
         print self.strfdelta(c, "Duration {hours}:{minutes}:{seconds}")
-        fd.write(self.strfdelta(c, "Duration {hours}:{minutes}:{seconds}") + "\n")
+        if fd is not None:
+            fd.write(self.strfdelta(c, "Duration {hours}:{minutes}:{seconds}") + "\n")
 
     def start(self):
+        global input
         self.enable = True
         filename = "TR" + str(datetime.date.today())
         home = expanduser("~")
@@ -37,20 +63,23 @@ class Tracker:
             os.makedirs(home + "/TR")
 
         while self.enable:
-            task = raw_input("Enter to Start timing:")
+            input = None
+            task = raw_input("Enter the project ur working,q to quit:")
 
             if task == 'Q' or task == 'q':
                 print "get the fuck out of here"
                 return
 
             a = datetime.datetime.now()
-            print "Working in %s" %(task)
+            t1 = threading.Thread(target=timer, args=(task, ))
+            t1.start()
             input = raw_input()
-            b = datetime.datetime.now()
 
+            b = datetime.datetime.now()
             with open(home + "/TR/" + filename, "a") as f:
                 f.write(task + "   ")
                 self.cal(a, b, f)
+            t1.join()
 
 if __name__ == "__main__":
         tracker = Tracker()
